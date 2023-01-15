5fa7cff302ef3f674c18bcf3d3471286c521149d
Myriam <myrjiam.sanchez@gmail.com>
2023-01-15 01:26:10 -0600

    commit Archivo Calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -17,3 +17,36 @@ class Operaciones:
         second_number = float(input('Ingresa el segundo número : '))
         resta = first_number - second_number
         print('La RESTA es: '+ str(resta))
+        
+    def multiplicar(self):
+        print('\n------------ MULTIPLICACIÓN ------------')
+        first_number = float(input('Ingresa el primer número : '))
+        second_number = float(input('Ingresa el segundo número : '))
+        multiplicacion = first_number * second_number
+        print('La MULTIPLICACIÓN es: '+ str(multiplicacion))
+        print('---------------------------------------- \n')
+
+    def dividir(self):
+        print('\n------------ DIVISIÓN ------------')
+        first_number = float(input('Ingresa el primer número : '))
+        second_number = float(input('Ingresa el segundo número : '))
+        division = first_number / second_number
+        print('La DIVISIÓN es: '+ str(division))
+        print('---------------------------------------- \n')
+    
+    def raiz(self):
+        print('\n------------ RAIZ n ------------')
+        numero = float(input('Ingresa el número a calcular : '))
+        grado = float(input('Ingresa el grado de la raíz: '))
+        raizn = pow(numero,1/grado)
+        print('La RAIZ '+ str(grado) + 'a de ' + str(numero) + ' es : '+ str(raizn))
+        print('---------------------------------------- \n')
+
+    def exponente(self):
+        print('\n------------ EXPONENTE n ------------')
+        numexp = float(input('Ingresa el número a calcular : '))
+        potencia = float(input('Ingresa el número de potencia: '))
+        #exponente = numexp ** potencia
+        exponente = np.power(numexp,potencia)
+        print('El EXPONENTE de '+str(numexp)+ ' es : '+ str(exponente))
+        print('---------------------------------------- \n')
