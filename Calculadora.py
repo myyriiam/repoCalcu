99f4a0ace611496c119b34470a512f833fe00917
Myriam <myrjiam.sanchez@gmail.com>
2023-01-15 01:53:01 -0600

    Operaciones 7 y8

--- Calculadora.py
+++ Calculadora.py
@@ -50,20 +50,19 @@ class Operaciones:
         exponente = np.power(numexp,potencia)
         print('El EXPONENTE de '+str(numexp)+ ' es : '+ str(exponente))
         print('---------------------------------------- \n')
-   
-    def exponente(self):
-        print('\n------------ EXPONENTE n ------------')
-        numexp = float(input('Ingresa el número a calcular : '))
-        potencia = float(input('Ingresa el número de potencia: '))
-        #exponente = numexp ** potencia
-        exponente = np.power(numexp,potencia)
-        print('El EXPONENTE de '+str(numexp)+ ' es : '+ str(exponente))
-        print('---------------------------------------- \n')
-
+    
     def seno(self):
         print('\n------------ SENO ------------')
         numseno = float(input('Ingresa el angulo en grados : '))
         radianes = (numseno * math.pi) / 180
         seno = math.sin(radianes)
         print('El SENO de '+str(numseno)+ ' es : '+ str(seno))
-        print('---------------------------------------- \n')    
+        print('---------------------------------------- \n')
+    
+    def coseno(self):
+        print('\n------------ COSENO ------------')
+        numcos = float(input('Ingresa el angulo en grados : '))
+        rad = (numcos * math.pi) / 180
+        coseno = math.cos(rad)
+        print('El COSENO de '+str(numcos)+ ' es : '+ str(coseno))
+        print('---------------------------------------- \n')
